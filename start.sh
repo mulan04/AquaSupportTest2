b493589355372f32433f24ff97029a2fc984d7b7
Andreas Müller <andreas.muller@aquasec.com>
2024-04-08 17:24:25 +0200

    Signed-off-by: Andreas Müller <andreas.muller@aquasec.com>

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-java -jar /app/malicious-server.jar &
-
-./bin/catalina.sh run
---
+++ test.py
@@ -0,0 +1,12 @@
+import requests
+
+api_client = requests.Session()
+
+response = api_client.post(
+    f"/v1/installments/{{installment.external_id}}/postpone",
+    json={"postpone_until": {"postpone_until": "postpone_until.strftime(YYYY_MM_DD_FORMAT)"}},
+)
+
+print(response.status_code)
+print(response.json())
+
