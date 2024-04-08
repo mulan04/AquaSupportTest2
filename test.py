17cbe11fe0e573050e6f55124de832bfce9b51ec
Andreas Müller <andreas.muller@aquasec.com>
2024-04-08 17:48:57 +0200

    Signed-off-by: Andreas Müller <andreas.muller@aquasec.com>

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ response = api_client.post(
     f"/v1/installments/{{installment.external_id}}/postpone",
     json={"postpone_until": {"postpone_until": "postpone_until.strftime(YYYY_MM_DD_FORMAT)"}},
 )
-
+#
 
 print(response.status_code)
 print(response.json())
